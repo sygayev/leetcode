8b0779515ed3dd01e6260daaf4c1c18a82cfda6c
Arman Sygayev <sygayev13@gmail.com>
2023-04-20 15:18:11 +0600

    3 task

--- 3Two Sum.py
+++ 3Two Sum.py
@@ -1,4 +1,8 @@
-nums = [2,7,11,15], target = 9
-for i in num: 
-            if i == target:
-print(i)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
